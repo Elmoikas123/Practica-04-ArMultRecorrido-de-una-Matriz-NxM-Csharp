5c42c703c0e736433c71b2ebf54ba70a509ceae6
mollo <mollo@PCMOIKAS>
2023-10-03 23:22:21 -0600

    Agregar archivos de proyecto.

---
+++ Practica 04 ArMultRecorrido de una Matriz NxM Csharp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    const int maxf = 3;
+    const int maxc = 5;
+
+    static void Main(string[] args)
+    {
+        float[,] a = new float[maxf, maxc];
+
+        // Leer valores en el array
+        for (int f = 0; f < maxf; f++)
+        {
+            for (int c = 0; c < maxc; c++)
+            {
+                Console.Write($"Ingresa el valor para la fila {f + 1}, columna {c + 1}: ");
+                if (float.TryParse(Console.ReadLine(), out float valor))
+                {
+                    a[f, c] = valor;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, ingresa un valor flotante válido.");
+                    // Decrementar los contadores del bucle para volver a intentar la entrada actual
+                    f--;
+                    c--;
+                }
+            }
+        }
+
+        // Mostrar el array
+        Console.WriteLine("Array:");
+        for (int f = 0; f < maxf; f++)
+        {
+            for (int c = 0; c < maxc; c++)
+            {
+                Console.Write(a[f, c] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Array:");
+        for (int f = 0; f < maxf; f++)
+        {
+            for (int c = 0; c < maxc; c++)
+            {
+                Console.Write(a[f, c] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Agregar una pausa antes de salir
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+
+}
+
